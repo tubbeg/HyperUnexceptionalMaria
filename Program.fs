8dc04b1d3b93dde0cb2e7cab1a9515e3d3f1b89a
Tobias Gärdebro <tubbeg@gmail.com>
2024-11-17 16:27:34 +0100

    First commit

---
+++ Program.fs
@@ -0,0 +1,15 @@
+﻿open Browser
+open Fable.Core
+
+type Position = float * float
+type Entity =
+    {position:Position;}
+
+
+[<Import("entity","./Entity.js")>]
+let entity : Entity = jsNative
+
+
+let div = document.createElement "div"
+div.innerHTML <- "hello hello hello!"
+document.body.appendChild div |> ignore
